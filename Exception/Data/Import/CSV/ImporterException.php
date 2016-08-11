32f134b533e301185c923870fd0d875ec63cd9e6
Roman Kulichkov <roman@kulichkov.pro>
2016-08-11 17:46:27 +0700

    Удалил корневую папку YourTools из репазитория

---
+++ Exception/Data/Import/CSV/ImporterException.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Your\Exception\Data\Import\CSV;
+
+use Your\Common\Exception;
+
+/**
+ * Исключение процесса выполнения импорта
+ *
+ * Class ImportException
+ *
+ * @author Roman Kulichkov <roman@kulichkov.pro>
+ *
+ * @package Your\Exception\Data\Import\CSV
+ */
+class ImporterException extends Exception
+{
+}
